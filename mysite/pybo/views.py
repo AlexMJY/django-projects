ca27bed2a02567830e4d1c27819f5d31bff53d90
EdwardYoungMoon <jmoon0227@gmail.com>
2022-12-27 21:52:22 +0900

    url alias, static

--- mysite/pybo/views.py
+++ mysite/pybo/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, redirect, render
 from .models import Question, Answer
+from django.utils import timezone
 
 def index(request):
     question_list = Question.objects.order_by('-create_dt')
@@ -10,3 +11,10 @@ def detail(request, question_id):
     question = Question.objects.get(id = question_id)
     context = {'question' : question}
     return render(request, 'pybo/question_detail.html', context)
+
+def answer_create(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    question.answer_set.create(content=request.POST.get('content'), create_dt = timezone.now()) 
+    # Question은 모델 Answer와 foreignkey로 연결되어 있기 때문에 위처럼 사용할 수 있다
+    return redirect('pybo:detail', question_id=question.id) # 질문을 생성한 후 상세 화면을 다시 보여주기 위해 redirect 함수 사용
+    
