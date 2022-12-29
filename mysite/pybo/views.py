98fe3548f907626f6a5ec141e6bc700adc446d45
EdwardYoungMoon <jmoon0227@gmail.com>
2022-12-29 19:23:25 +0900

    paginator

--- mysite/pybo/views.py
+++ mysite/pybo/views.py
@@ -3,10 +3,14 @@ from .models import Question, Answer
 from django.utils import timezone
 from .forms import QuestionForm, AnswerForm
 from django.http import HttpResponseNotAllowed
+from django.core.paginator import Paginator
 
 def index(request):
+    page = request.GET.get('page', '1') # GET방식으로 호출된 URL에서 page값을 가져온다. page값이 없이 호출된 경웨 default로 1을 설정
     question_list = Question.objects.order_by('-create_dt')
-    context = {'question_list' : question_list}
+    paginator = Paginator(question_list, 10) # 페이지당 10개씩 보여주기
+    page_obj = paginator.get_page(page) # 요청된 페이지에 해당되는 객체 생성. 장고 내부적으로 데이터 전체를 조회하지 않고 해당 페이지의 데이터만 조회하도록 쿼리가 변경된다.
+    context = {'question_list' : page_obj}
     return render(request, 'pybo/question_list.html', context)
 
 def detail(request, question_id):
