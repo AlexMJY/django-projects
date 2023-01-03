6c840db6f2fb0c34ae00a300b8297ca1f463778e
EdwardYoungMoon <jmoon0227@gmail.com>
2023-01-03 22:36:12 +0900

    add author(model)

--- mysite/pybo/models.py
+++ mysite/pybo/models.py
@@ -1,9 +1,13 @@
+# 모델을 변경한 후에는 반드시 makemigrations와 migrate를 통해 데이터베이스를 변경해야 한다.
+
 from django.db import models
+from django.contrib.auth.models import User
 
 class Question(models.Model):
     subject = models.CharField(max_length=200)
     content = models.TextField()
     create_dt = models.DateTimeField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
     
     def __str__(self): # admin 사이트에 subject 보이기
         return self.subject
@@ -13,3 +17,4 @@ class Answer(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     content = models.TextField()
     create_dt = models.DateTimeField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
--- mysite/pybo/views.py
+++ mysite/pybo/views.py
@@ -4,6 +4,7 @@ from django.utils import timezone
 from .forms import QuestionForm, AnswerForm
 from django.http import HttpResponseNotAllowed
 from django.core.paginator import Paginator
+from django.contrib.auth.decorators import login_required
 
 def index(request):
     page = request.GET.get('page', '1') # GET방식으로 호출된 URL에서 page값을 가져온다. page값이 없이 호출된 경웨 default로 1을 설정
@@ -18,27 +19,30 @@ def detail(request, question_id):
     context = {'question' : question}
     return render(request, 'pybo/question_detail.html', context)
 
+@login_required(login_url='common:login') # 아래의 함수는 로그인이 필요하다.
 def answer_create(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     if request.method == "POST":
         form = AnswerForm(request.POST)
         if form.is_valid():
             answer = form.save(commit=False)
+            answer.author = request.user # # author 속성에 로그인 계정 저장. request.user는 현재 로그인한 계정의 User 모델 객체이다.
             answer.create_dt = timezone.now()
             answer.question = question
             answer.save()
             return redirect('pybo:detail', question_id=question.id) # 질문을 생성한 후 상세 화면을 다시 보여주기 위해 redirect 함수 사용
     else:
-        return HttpResponseNotAllowed("Only POST is possible.")
+        form = AnswerForm()
     context = {'question' : question, 'form' : form}
     return render(request, 'pybo/question_detail.html', context)
 
-    
+@login_required(login_url='common:login') # 아래의 함수는 로그인이 필요하다.
 def question_create(request):
     if request.method == 'POST':
         form = QuestionForm(request.POST)
         if form.is_valid(): # 폼이 유효하다면
             question = form.save(commit=False) # 임시 저장하여 question 객체를 리턴받는다.
+            question.author = request.user
             question.create_dt = timezone.now() # 실제 저장을 위해 작성일시를 설정한다.
             question.save() # 데이터를 실제로 저장한다.
             return redirect('pybo:index')
