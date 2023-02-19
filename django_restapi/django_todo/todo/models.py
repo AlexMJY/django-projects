ebd4f5f02c1c8c521198613475b38899eafe3f59
Alex <alex@Alexui-MacBookAir.local>
2023-02-19 20:50:32 +0900

    django restapi-todo models.py, todo_list.html

--- django_restapi/django_todo/todo/models.py
+++ django_restapi/django_todo/todo/models.py
@@ -1,11 +1,12 @@
 from django.db import models
 
 class Todo(models.Model):
-    title = models.CharField(max_length=50)
-    description = models.TextField(max_length=200)
-    created = models.DateTimeField(auto_now_add=True)
+    # django가 기본으로 제공하는 pk인 id 필드도 포함되어 있다.
+    title = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    created = models.DateTimeField(auto_now_add=False)
     complete = models.BooleanField(default=False)
-    importance = models.BooleanField(default=False)
+    important = models.BooleanField(default=False)
     
     def __str__(self):
         return self.title
