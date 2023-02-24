6e8c3d5b9d581bf2e3014aab4224f63a4e988a96
Alex-MJY <jmoon0227@gmail.com>
2023-02-25 00:43:00 +0900

    drf_tutorial - settings(views, urls, installed_apps) git push origin main

--- drf_tutorial/tutorial/tutorial/quickstart/views.py
+++ drf_tutorial/tutorial/tutorial/quickstart/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User, Group
+from rest_framework import viewsets, permissions
+from tutorial.quickstart.serializers import UserSerializer, GroupSerializer
 
-# Create your views here.
+class UserViewSet(viewsets.ModelViewSet):
+    # API endpoint that allows user to be viewed or edited.
+    queryset = User.objects.all().order_by('-dated_joined')
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    
+class GroupViewSet(viewsets.ModelViewSet):
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+    permission_classes = [permissions.IsAuthenticated]
