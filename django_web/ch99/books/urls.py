bff20f95d1f8909f3239b86b55661232e8122373
Alex-MJY <jmoon0227@gmail.com>
2023-03-12 14:35:45 +0900

    django_web book urls

--- django_web/ch99/books/urls.py
+++ django_web/ch99/books/urls.py
@@ -4,6 +4,11 @@ from . import views
 app_name = 'books' 
 
 urlpatterns = [
-    path('', views.BookList.as_view(), name='index'),
-    path()
+    path('', views.BooksModelView.as_view(), name='index'),
+    path('book/', views.BookList.as_view(), name='book_list'),
+    path('author/', views.AuthorList.as_view(), name='author_list'),
+    path('publisher/', views.PublisherList.as_view(), name='publisher_list'),
+    path('book/<int:pk>/', views.BookDetail.as_view(), name='book_detail'),
+    path('author/<int:pk>/', views.AuthorDetail.as_view(), name='author_detail'),
+    path('publisher/<int:pk>/', views.PublisherDetail.as_view(), name='publisher_detail'),
 ]
