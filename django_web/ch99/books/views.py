7caa5d419ae0bcad2dad523de9801851c7e2b42a
Alex-MJY <jmoon0227@gmail.com>
2023-03-12 14:44:55 +0900

    django_web book views

--- django_web/ch99/books/views.py
+++ django_web/ch99/books/views.py
@@ -1,3 +1,35 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.views.generic.base import TemplateView
+from django.views.generic import ListView, DetailView
+from books.models import Book, Author, Publisher
+
+
+# TemplateView
+class BooksModelView(TemplateView):
+    template_name = 'books/index.html'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['model_list'] = ['Book', 'Author', 'Publihser'] # 애플리케이션 첫 화면에 테이블 리스트를 보여주기 위해 model_list에 담아서 템플릿 시스템에 넘겨준다.
+        return context
+
+# ListBiew
+class BookList(ListView):
+    model = Book
+    
+class AuthorList(ListView):
+    model = Author
+    
+class PublisherList(ListView):
+    model = Publisher
+    
+# DetailView
+class BookDetail(DetailView):
+    model = Book
+    
+class BookDetail(DetailView):
+    model = Author
+    
+class PublisherDetail(DetailView):
+    model = Publisher
