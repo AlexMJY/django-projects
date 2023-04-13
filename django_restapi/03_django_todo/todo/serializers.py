c603f66cdb1b6050812d2416915f66ce52548603
Alex-MJY <jmoon0227@gmail.com>
2023-04-13 22:44:15 +0900

    deleted unused folders

--- django_restapi/03_django_todo/todo/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import Todo
-
-class TodoSimpleSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Todo
-        fields = ('id', 'title', 'complete', 'important')
